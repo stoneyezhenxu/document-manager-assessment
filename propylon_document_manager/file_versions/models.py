84517478c1ac06972bb368cd7eb0718a3a18d3bd
stoneyezhenxu <stoneyezhenxu@gmail.com>
2023-11-22 18:51:46 +0000

    add file_version model

--- propylon_document_manager/file_versions/models.py
+++ propylon_document_manager/file_versions/models.py
@@ -1,5 +1,23 @@
 from django.db import models
+from django.conf import settings
+
+from propylon_document_manager.utils.global_value import GlobalValue
+from propylon_document_manager.utils.utils import get_uuid
+
 
 class FileVersion(models.Model):
-    file_name = models.fields.CharField(max_length=512)
-    version_number = models.fields.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    file_name = models.fields.CharField(max_length=512, verbose_name='file_name')
+    file_version = models.fields.IntegerField(verbose_name='file_version', default=0)
+    file_size = models.fields.CharField(max_length=20, verbose_name='file_size')
+    file_type = models.fields.CharField(choices=GlobalValue.FileTypes_choices, max_length=15, default='others',
+                                        verbose_name='file_type')
+    file_url = models.fields.CharField(max_length=512, verbose_name='file_url')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='create_time')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='update_time')
+    deleted_at = models.DateTimeField(auto_now=True, verbose_name='delete_time')
+    file_uuid = models.UUIDField(unique=True, default=get_uuid, verbose_name='file_uuid')
+    file_name_hash = models.fields.CharField(max_length=32, verbose_name='file_uuid')
+
+    def __str__(self):
+        return self.file_name
