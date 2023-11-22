6a6409e1f327614596e9fb139554ed93758b73ce
stoneyezhenxu <stoneyezhenxu@gmail.com>
2023-11-22 18:44:53 +0000

    add file_version serializers

--- propylon_document_manager/file_versions/api/serializers.py
+++ propylon_document_manager/file_versions/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 
 from propylon_document_manager.file_versions.models import FileVersion
+from propylon_document_manager.utils.global_value import GlobalValue
+
 
 class FileVersionSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileVersion
-        fields = "__all__"
+        fields = [GlobalValue.FileName, GlobalValue.FileVersion, GlobalValue.FileSize, GlobalValue.FileType,
+                  GlobalValue.FileUrl, GlobalValue.FileNameHash]
