4fbbf52edc859a27e82be5b5ddebe403e80e690e
stoneyezhenxu <stoneyezhenxu@gmail.com>
2023-11-24 18:26:46 +0000

    update utils

--- client/doc-manager/src/utils/index.js
+++ client/doc-manager/src/utils/index.js
@@ -1,2 +1,4 @@
 export { axiosAPI } from './axios'
+export { BASE_URL } from './url'
 export { TOKEN_NAME, getToken, setToken, removeToken, isAuth } from './auth'
+export { getFileHashCode } from './hash_md5'
