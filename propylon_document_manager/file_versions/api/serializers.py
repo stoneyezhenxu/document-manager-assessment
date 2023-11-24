884f7a1c083073f11974ba639f846a02383424f1
stoneyezhenxu <stoneyezhenxu@gmail.com>
2023-11-24 17:43:34 +0000

    add model & serializer

--- propylon_document_manager/file_versions/api/serializers.py
+++ propylon_document_manager/file_versions/api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-
 from file_versions.models import FileVersion
 from propylon_document_manager.utils.global_value import GlobalValue
 
@@ -8,4 +7,14 @@ class FileVersionSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileVersion
         fields = [GlobalValue.FileName, GlobalValue.FileVersion, GlobalValue.FileSize, GlobalValue.FileType,
-                  GlobalValue.FileUrl, GlobalValue.FileNameHash]
+                  GlobalValue.FileUrl, GlobalValue.FileNameHash, GlobalValue.FileDesc, GlobalValue.FileUuid]
+        # fields = "__all__"
+
+
+class FileVersionListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FileVersion
+        fields = [GlobalValue.FileName, GlobalValue.FileVersion, GlobalValue.FileSize, GlobalValue.FileType,
+                  GlobalValue.FileUrl, GlobalValue.FileNameHash, GlobalValue.FileDesc, GlobalValue.FileUuid,
+                  GlobalValue.FIleCreateTime]
+        # fields = "__all__"
--- propylon_document_manager/file_versions/models.py
+++ propylon_document_manager/file_versions/models.py
@@ -5,19 +5,22 @@ from propylon_document_manager.utils.global_value import GlobalValue
 from propylon_document_manager.utils.utils import get_uuid
 
 
+
+
 class FileVersion(models.Model):
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     file_name = models.fields.CharField(max_length=512, verbose_name='file_name')
-    file_version = models.fields.IntegerField(verbose_name='file_version', default=0)
+    file_version = models.fields.IntegerField(verbose_name='file_version',default=0)
     file_size = models.fields.CharField(max_length=20, verbose_name='file_size')
-    file_type = models.fields.CharField(choices=GlobalValue.FileTypes_choices, max_length=15, default='others',
-                                        verbose_name='file_type')
+    file_type = models.fields.CharField(choices=GlobalValue.FileTypes_choices, max_length=15, verbose_name='file_type',default='others')
     file_url = models.fields.CharField(max_length=512, verbose_name='file_url')
-    create_at = models.DateTimeField(auto_now_add=True, verbose_name='create_time')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='update_time')
-    deleted_at = models.DateTimeField(auto_now=True, verbose_name='delete_time')
+    create_at = models.DateTimeField(auto_now_add=True,verbose_name='create_time')
+    updated_at = models.DateTimeField(auto_now=True,verbose_name='update_time')
+    deleted_at = models.DateTimeField(auto_now=True,verbose_name='delete_time')
     file_uuid = models.UUIDField(unique=True, default=get_uuid, verbose_name='file_uuid')
     file_name_hash = models.fields.CharField(max_length=32, verbose_name='file_uuid')
+    file_desc = models.fields.CharField(max_length=512,default='', verbose_name='file_desc')
 
     def __str__(self):
         return self.file_name
