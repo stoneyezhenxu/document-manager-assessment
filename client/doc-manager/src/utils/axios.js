17b1650d7d2a4139e3cf6919e124d1bbc437cf11
stoneyezhenxu <stoneyezhenxu@gmail.com>
2023-11-24 18:23:44 +0000

    update axios

--- client/doc-manager/src/utils/axios.js
+++ client/doc-manager/src/utils/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 import { getToken, removeToken } from './auth'
 
-const accessToken = 'a5c328ce13cbc28f79e344ac8925462b22ff13b8'
 export const axiosAPI = axios.create({
-
     baseURL: process.env.REACT_APP_URL
 })
 
@@ -16,10 +14,10 @@ axiosAPI.interceptors.request.use((config) => {
         && !url.startsWith('/api/login')
         && !url.startsWith('/api/register')
     ) {
-        // config.headers.Authorization = getToken()
+        config.headers.Authorization = `Token ${getToken()}`
 
-        config.headers.Authorization = `Token ${accessToken}`
     }
+
 
     return config
 })
